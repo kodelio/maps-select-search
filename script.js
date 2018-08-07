0f00c5e315451a6a904c4ae8fe4901f6c6fa55e4
Laurent Toson <laurentt96@outlooK.fr>
2018-08-07 22:01:41 +0200

    fixed launch

--- script.js
+++ script.js
@@ -2,11 +2,18 @@
  * Created by Laurent on 02/06/2017.
  */
 
-// on launched event
-chrome.runtime.onLaunched.addListener(create);
+function create() {
+    var context = "selection";
+    var title = "Search on Google Maps";
+    chrome.contextMenus.create({"title": title, "contexts":[context],
+        "id": "context" + context});
+}
 
-// on installed event
-chrome.runtime.onInstalled.addListener(create);
+chrome.runtime.onInstalled.addListener(function() {
+    create();
+});
+
+create();
 
 // add click event
 chrome.contextMenus.onClicked.addListener(onClickHandler);
@@ -16,14 +23,4 @@ function onClickHandler(info, tab) {
     var sText = info.selectionText;
     var url = "https://www.google.com/maps?q=" + encodeURIComponent(sText);
     window.open(url, '_blank');
-}
-
-// create the context menu
-function create() {
-    var context = "selection";
-    var title = "Search on Google Maps";
-    chrome.contextMenus.create({
-        "title": title, "contexts": [context],
-        "id": "context" + context
-    });
-}
+};
