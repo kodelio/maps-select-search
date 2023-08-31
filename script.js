f466034710cdea0dd58a64520599f77b3d38ef6c
Laurent Toson <9209206+kokno@users.noreply.github.com>
2023-08-31 13:23:37 +0200

    update to 2.0 (manifest v3)

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-/**
- * Created by Laurent on 02/06/2017.
- */
-
-function create() {
+chrome.runtime.onInstalled.addListener(() => {
   var context = "selection";
   var title = "Search this address on Google Maps";
   chrome.contextMenus.create({
@@ -10,20 +6,10 @@ function create() {
     contexts: [context],
     id: "context" + context,
   });
-}
-
-chrome.runtime.onInstalled.addListener(function () {
-  create();
 });
 
-create();
-
-// add click event
-chrome.contextMenus.onClicked.addListener(onClickHandler);
-
-// The onClicked callback function.
-function onClickHandler(info, tab) {
+chrome.contextMenus.onClicked.addListener((info, tab) => {
   var sText = info.selectionText;
   var url = "https://www.google.com/maps?q=" + encodeURIComponent(sText);
-  window.open(url, "_blank");
-}
+  chrome.tabs.create({ url: url });
+});
